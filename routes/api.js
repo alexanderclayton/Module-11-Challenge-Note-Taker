5f0ae19b159df1c0cfa9ebe2907792b394cfb9e3
Alex Clayton <alexander.clayton.email@gmail.com>
2022-11-29 00:44:37 -0600

    connected the dots

---
+++ routes/api.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const uuid = require('../helpers/uuid')
+
+module.exports = app => {
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+
+        app.get('/api/notes', (req, res) => {
+            res.json(notes)
+        });
+
+        app.post('/api/notes', (req, res) => {
+            let newNote = {
+                ...req.body,
+                id: uuid(),
+            }
+            notes.push(newNote);
+            writeNotes();
+        });
+
+        app.delete('/api/notes/:id', (req, res) => {
+            notes.splice(req.params.id, 1);
+            writeNotes();
+        });
+
+        function writeNotes() {
+            fs.writeFile('./db/db.json', JSON.stringify(notes, 'utf8', err => {
+                if (err) throw err;
+                return true;
+            }));
+        }
+    });
+};
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-const { application } = require('express');
 const express = require('express');
-const path = require('path');
 const PORT = 3001;
 
 const app = express();
@@ -10,9 +8,8 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
+require('./routes/api.js')(app);
+require('./routes/html.js')(app);
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
