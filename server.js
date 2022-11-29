6efb6f1b7145a419022dbde11aacf63064c0c003
Alex Clayton <alexander.clayton.email@gmail.com>
2022-11-28 22:36:04 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const { application } = require('express');
+const express = require('express');
+const path = require('path');
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
