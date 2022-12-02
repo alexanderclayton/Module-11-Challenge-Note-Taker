3dde3791ff5c8804db166c194f094bdd4af93b5b
Alex Clayton <alexander.clayton.email@gmail.com>
2022-12-01 22:13:36 -0600

    actually fixed bugs

--- routes/api.js
+++ routes/api.js
@@ -2,34 +2,37 @@ const fs = require('fs');
 const uuid = require('../helpers/uuid')
 
 module.exports = app => {
+    let notes;
 
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
-        const notes = JSON.parse(data);
-
-        app.get('/api/notes', (req, res) => {
-            res.json(notes)
-        });
-
-        app.post('/api/notes', (req, res) => {
-            let newNote = {
-                ...req.body,
-                id: uuid(),
-            }
-            notes.push(newNote);
-            writeNotes();
-        });
-
-        app.delete('/api/notes/:id', (req, res) => {
-            notes.splice(req.params.id, 1);
-            writeNotes();
-        });
-
-        function writeNotes() {
-            fs.writeFile('./db/db.json', JSON.stringify(notes), 'utf8', err => {
-                if (err) throw err;
-                return true;
-            });
-        }
+        notes = JSON.parse(data);
     });
+
+    app.get('/api/notes', (req, res) => {
+        res.json(notes)
+    });
+
+    app.post('/api/notes', (req, res) => {
+        let newNote = {
+            ...req.body,
+            id: uuid(),
+        }
+        notes.push(newNote);
+        writeNotes();
+        res.json(notes)
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        notes.splice(req.params.id, 1);
+        writeNotes();
+        res.json(notes)
+    });
+
+    function writeNotes() {
+        fs.writeFile('./db/db.json', JSON.stringify(notes), 'utf8', err => {
+            if (err) throw err;
+            return true;
+        });
+    }
 };
